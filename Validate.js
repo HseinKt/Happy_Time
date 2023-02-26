489bc24069747f8b14bbb86954548094bb4a035c
HseinKt <hseinkteish1998@gmail.com>
2023-02-26 19:23:13 +0200

    validated the password in the register form in a specific style.

--- Validate.js
+++ Validate.js
@@ -1,11 +1,33 @@
 
 const name = document.getElementById("name");
-const username = document.getElementById("username").value;
+const username = document.getElementById("username");
 const email = document.getElementById("email");
-const password = document.getElementById("password").value;
+const password = document.getElementById("password");
+const repeat_password = document.getElementById("password2");
 const submit = document.getElementById("submit");
+const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+const upperCase = /[A-Z]/
 
-submit.addEventListener('click', () => {
-    console.log(name.value);
-    alert(email.value)
+submit.addEventListener('click', (e) => {
+    if (name.value == ""||username.value == ""||email.value == ""||password.value == ""||repeat_password.value == "") {
+        alert("some of your data still empty. please ");
+    }
+    else if (password.value.length < 8 || repeat_password.value.length < 8) {
+        alert("Password should contain 8 characters minimum");
+    }
+    else if (!specialChars.test(password.value) ) {
+        alert("Password should contain at least one special character");
+    }
+    else if (!upperCase.test(password.value) ) {
+        console.log("upperCase")
+        alert("Password should contain at least one upper case letter");
+    }
+    else if (password.value != repeat_password.value){
+        console.log(password.value)
+        console.log(repeat_password.value)
+        alert("Please make sure to repeat the same password");
+    }
+    
 })
+
+
