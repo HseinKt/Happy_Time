d94d1a7c76747975c9a996dbea43d4dc6ab0ecbc
HseinKt <hseinkteish1998@gmail.com>
2023-02-26 23:51:52 +0200

    added the merge_sort function

--- Validate.js
+++ Validate.js
@@ -6,9 +6,30 @@ const password = document.getElementById("password");
 const repeat_password = document.getElementById("password2");
 const submit = document.getElementById("submit");
 const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-const upperCase = /[A-Z]/
-const Email_pattern=/[a-z0-9]+@[a-z0-9.-]+\.[a-z]{3,}$/
+const upperCase = /[A-Z]/;
+const Email_pattern=/[a-z0-9]+@[a-z0-9.-]+\.[a-z]{3,}$/;
+const smallBox = document.getElementById("smallBox");
 
+function merge_Arrays(left_sub_array, right_sub_array) {
+    let array = []
+    while (left_sub_array.length && right_sub_array.length) {
+       if (left_sub_array[0] < right_sub_array[0]) {
+          array.push(left_sub_array.shift())
+       } else {
+          array.push(right_sub_array.shift())
+       }
+    }
+    return [ ...array, ...left_sub_array, ...right_sub_array ]
+}
+
+function merge_sort(Array) {
+    const middle_index = Array.length / 2
+    if(Array.length < 2) {
+       return Array
+    }
+    const left_sub_array = Array.splice(0, middle_index)
+    return merge_Arrays(merge_sort(left_sub_array),merge_sort(Array))
+}
 
 submit.addEventListener('click', (e) => {
     const obj={
@@ -25,7 +46,7 @@ submit.addEventListener('click', (e) => {
     if (name.value == ""||username.value == ""||email.value == ""||password.value == ""||repeat_password.value == "") {
         alert("some of your data still empty. please ");
     }
-    else if (password.value.length < 8 || repeat_password.value.length < 8) {
+    else if (password.value.length < 8) {
         alert("Password should contain 8 characters minimum"); 
     }
     else if (!specialChars.test(password.value) ) {
@@ -43,5 +64,16 @@ submit.addEventListener('click', (e) => {
     else if (!Email_pattern.test(email.value)){
         alert("Please enter a valid email address.");
     }
+    else {
+        //location.replace("mergeSort.html")
+        var Val = prompt("please add 10 numbers  : ", "39 28 44 4 10 83 11");
+        var Array = Val.split(" ").map(function(item) {
+            return parseInt(item, 10);
+        });
 
+        console.log("You entered : " + merge_sort(Array));
+    }
 })
+
+
+
